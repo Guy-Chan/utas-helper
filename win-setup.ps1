62c9a5f33f3caf93794f2259ca078bd243c054e9
Guy-Chan <107745757+Guy-Chan@users.noreply.github.com>
2023-09-15 19:05:12 +1000

    Update win-setup.ps1 - fix PT setting restore
    * repo should be cloned before restore PT setting

--- win-setup.ps1
+++ win-setup.ps1
@@ -64,19 +64,21 @@ scoop bucket add extras
 scoop bucket add nerd-fonts
 
 scoop install aria2
-
 scoop install powertoys
+git clone https://github.com/Guy-Chan/utas-helper.git "$env:repos/utas-helper"
 unzip -o "$env:repos\utas-helper\PowerToys-setting.zip" -d "$env:LOCALAPPDATA\Microsoft"
 start -FilePath "$env:APPDATA\Microsoft\Windows\Start Menu\Programs\Scoop Apps\PowerToys.lnk" -WindowStyle Hidden
+$wt_settings_path = $(ls "${env:LOCALAPPDATA}/Packages/Microsoft.WindowsTerminal_*/LocalState/settings.json" | Select-Object -Property FullName).FullName
+cp "$env:repos/utas-helper/wt-setting.json" "$wt_settings_path"
+
+scoop install snipaste
+start -FilePath "$env:APPDATA\Microsoft\Windows\Start Menu\Programs\Scoop Apps\Snipaste.lnk" -WindowStyle Hidden
 
 scoop install firefox
 scoop install potplayer
 scoop install gh
 scoop install clash
 scoop install zoom
-scoop install snipaste
-start -FilePath "$env:APPDATA\Microsoft\Windows\Start Menu\Programs\Scoop Apps\Snipaste.lnk" -WindowStyle Hidden
-
 scoop install logseq
 scoop install calibre-normal
 scoop install aria-ng-gui
@@ -85,15 +87,13 @@ scoop install sysinternals
 scoop install yt-dlp
 scoop install neofetch
 scoop install winget
-scoop install oh-my-posh
-scoop install CascadiaCode-NF-Mono
 scoop install zip
 scoop install tealdeer
 tldr --update
 if (-not(Test-Path ~/repos)) { mkdir ~/repos/utas -Force }
-git clone https://github.com/Guy-Chan/utas-helper.git "$env:repos/utas-helper"
-$wt_settings_path = $(ls "${env:LOCALAPPDATA}/Packages/Microsoft.WindowsTerminal_*/LocalState/settings.json" | Select-Object -Property FullName).FullName
-cp "$env:repos/utas-helper/wt-setting.json" "$wt_settings_path"
+
+scoop install oh-my-posh
+scoop install CascadiaCode-NF-Mono
 echo 'oh-my-posh init pwsh | Invoke-Expression' | ac $PROFILE
 . $PROFILE
     
