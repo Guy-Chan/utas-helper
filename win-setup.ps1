2f1d0514b23e9114a7fbb5bb4fe979583e1da99f
Guy-Chan <107745757+Guy-Chan@users.noreply.github.com>
2024-09-02 16:17:01 +1000

    Update win-setup.ps1

--- win-setup.ps1
+++ win-setup.ps1
@@ -22,8 +22,8 @@ Set-Alias wl-copy Set-Clipboard
 # Functions for convenience
 function helper() { cd "$env:repos/utas-helper" }
 function utas() { ."$env:ComSpec/../whoami*" /upn | wl-copy } # set utas email to the clipboard, can be quite handy sometimes
-function 501() { ssh "$env:USERNAME@ictteach.its.utas.edu.au" } # ssh to KIT501 lab server
-function 502() { ssh "$env:USERNAME@ictteach-www.its.utas.edu.au" } # ssh to KIT502 lab server
+function k501() { ssh "$env:USERNAME@ictteach.its.utas.edu.au" } # ssh to KIT501 lab server
+function k502() { ssh "$env:USERNAME@ictteach-www.its.utas.edu.au" } # ssh to KIT502 lab server
 '@ | ac $PROFILE
 . $PROFILE
 
