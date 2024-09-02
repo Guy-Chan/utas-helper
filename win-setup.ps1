e5d55bd06335fd81d54edf321c5f2b09103d232b
Guy-Chan <107745757+Guy-Chan@users.noreply.github.com>
2024-09-02 15:56:09 +1000

    Update win-setup.ps1

--- win-setup.ps1
+++ win-setup.ps1
@@ -1,9 +1,13 @@
-$ErrorActionPreference = 'silentlycontinue'
-if (-not(Test-Path $PROFILE)) { mkdir "$PROFILE/.." -Force }
+$ErrorActionPreference = 'SilentlyContinue'
+# Ensure the PowerShell profile directory exists
+if (-not (Test-Path $PROFILE)) { 
+    mkdir (Split-Path -Parent $PROFILE) -Force 
+}
 
-rm $PROFILE -Force
+# Remove existing profile and create a new one
+Remove-Item $PROFILE -Force
 echo @'
-# set env
+# Set environment variables and aliases
 Set-PSReadLineOption -EditMode Emacs
 $env:scoop = "$env:USERPROFILE\scoop"
 $env:repos = "$env:USERPROFILE/repos"
@@ -13,47 +17,29 @@ $env:path = ";$env:scoop\apps\git\current\mingw64\bin"`
 + ";$env:scoop\shims"`
 + ";$env:path"
 
-set-alias wl-copy set-clipboard
+Set-Alias wl-copy Set-Clipboard
 
+# Functions for convenience
 function helper() { cd "$env:repos/utas-helper" }
-function misc() { cd "$env:utas_repos/utas-misc" }
-function utas() { ."$env:ComSpec/../whoami*" /upn | wl-copy }
-function ict() { ssh "$env:USERNAME@ictteach.its.utas.edu.au" }
-function tracker() { echo 'https://raw.githubusercontent.com/ngosang/trackerslist/master/trackers_best.txt' | wl-copy }
-function jetbrains() { 
-    winget install 'JetBrains.IntelliJIDEA.Ultimate'
-    winget install 'JetBrains.PyCharm.Professional'
-}
-
-function sync($git_dir){
-    echo "start sync the repo located at ${git_dir} ."
-    git -C "$git_dir" fo
-    git -C "$git_dir" pull
-    git -C "$git_dir" add -A
-    git -C "$git_dir" ci -m 'sync'
-    git -C "$git_dir" po
-    echo "end sync the repo located at ${git_dir} ."
-}
+function utas() { ."$env:ComSpec/../whoami*" /upn | wl-copy } # set utas email to the clipboard, can be quite handy sometimes
+function 501() { ssh "$env:USERNAME@ictteach.its.utas.edu.au" } # ssh to KIT501 lab server
+function 502() { ssh "$env:USERNAME@ictteach-www.its.utas.edu.au" } # ssh to KIT502 lab server
 '@ | ac $PROFILE
 . $PROFILE
 
-$ErrorActionPreference = 'SilentlyContinue'
+# Configure VS Code if not already set
 $cmdlet = Get-Command -Name "code" -ErrorAction SilentlyContinue
-if ($cmdlet -eq $null) {
+if (-not $cmdlet) {
     echo @'
-# add vscode to env
+# Add VS Code to PATH
 $env:path = ";$env:ProgramFiles\Microsoft VS Code\bin"`
 + ";$env:path"
 '@ | ac $PROFILE
     . $PROFILE
 }
 
-# allow policy
-if (-not(Test-Path $env:scoop)) {
-    iwr -useb get.scoop.sh | iex
-}
-
-function code_extensions_install() {
+# Install Visual Studio Code extensions
+function code_extensions_install {
     code --install-extension ms-vscode.powershell
     code --install-extension GitHub.copilot
     code --install-extension ms-python.debugpy
@@ -64,6 +50,12 @@ function code_extensions_install() {
 
 code_extensions_install
 
+# Scoop and additional setups
+if (-not(Test-Path $env:scoop)) {
+    iwr -useb get.scoop.sh | iex
+}
+
+# Install and configure Git
 scoop install git
 git config --global alias.fu 'fetch upstream'
 git config --global alias.fo 'fetch origin'
@@ -82,40 +74,35 @@ git config --global pull.rebase false
 
 scoop bucket add extras
 scoop bucket add nerd-fonts
-
 scoop install aria2
 scoop config aria2-warning-enabled false
 
 scoop install powertoys
 git clone https://github.com/Guy-Chan/utas-helper.git "$env:repos/utas-helper"
 
-scoop install jq jid marp zip pandoc carnac sysinternals yt-dlp msys2 fastfetch dua
-scoop install oh-my-posh CascadiaCode-NF-Mono 
-scoop install zoom
+scoop install jq jid marp zip pandoc carnac sysinternals yt-dlp msys2 fastfetch dua oh-my-posh CascadiaCode-NF-Mono zoom
 
-# cp the backup PT settings  
+# Restore PowerToys settings
 $ptb="~\Documents\PowerToys\Backup\*ptb"
-if (-not(Test-Path $ptb)) { 
+if (-not (Test-Path $ptb)) { 
     mkdir "$ptb/.." -Force 
     cp "$env:repos\utas-helper\*ptb" "$ptb/.."
 }
 
-# restore msys2 .profile
+# Restore msys2 profile
 cp "$env:repos\utas-helper\msys2-profile" "$env:scoop\apps\msys2\current\home\$(whoami)\.profile" 
   
-# Terminate the PowerToys process  
+# Restart PowerToys
 Get-Process PowerToys -ErrorAction SilentlyContinue | Stop-Process  
-  
-# Start PowerToys  
 start -FilePath "$env:APPDATA\Microsoft\Windows\Start Menu\Programs\Scoop Apps\PowerToys.lnk" -WindowStyle Hidden  
 
+# Initialize oh-my-posh
 echo 'oh-my-posh init pwsh | Invoke-Expression' | ac $PROFILE
 
-# restore wt settings.json
+# Restore Windows Terminal settings
 $wt_settings_path = $(ls "${env:LOCALAPPDATA}/Packages/Microsoft.WindowsTerminal_*/LocalState/settings.json" | Select-Object -Property FullName).FullName
 cp "$env:repos/utas-helper/wt-setting.json" "$wt_settings_path"
 
-# restore wt state.json
 $wt_state_path = $(ls "${env:LOCALAPPDATA}/Packages/Microsoft.WindowsTerminal_*/LocalState/state.json" | Select-Object -Property FullName).FullName
 $json = Get-Content -Path "$wt_state_path" | ConvertFrom-Json  
 # Check if the dismissedMessages property already exists  
@@ -133,12 +120,7 @@ if ($gh_token) {
     customization
 }
 
-# reload WindowsTerminal
-# Get the current WindowsTerminal process  
+# Restart Windows Terminal
 $currentProcess = Get-Process WindowsTerminal -ErrorAction SilentlyContinue | Select-Object -First 1  
-  
-# Start a new WindowsTerminal process  
 Start-Process wt  
-  
-# Stop the current WindowsTerminal process  
 exit
